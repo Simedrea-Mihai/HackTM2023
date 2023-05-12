adfb385488703feb3b2b39c57770f1b9b425ce9e
Simedrea-Mihai <mihai.nicolae.simedrea@gmail.com>
2023-05-12 23:30:26 +0300

    Implement geolocation

--- FE/src/app/shared/components/map/map.component.ts
+++ FE/src/app/shared/components/map/map.component.ts
@@ -7,25 +7,40 @@ import { Map, NavigationControl } from 'maplibre-gl';
 	styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
-	ngOnInit(): void {
-  }
-
 	map: Map | undefined;
 
 	@ViewChild('map')
 	private mapContainer!: ElementRef<HTMLElement>;
 
-	ngAfterViewInit() {
-		const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+	ngOnInit(): void {}
 
-		this.map = new Map({
-			container: this.mapContainer.nativeElement,
-			style: `https://api.maptiler.com/maps/streets-v2/style.json?key=97sou0kVjlk5MxdovBEU `,
-			center: [initialState.lng, initialState.lat],
-			zoom: initialState.zoom
-		});
-    
-    this.map?.addControl(new NavigationControl({}), 'top-right');
+	ngAfterViewInit() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					const latitude = position.coords.latitude;
+					const longitude = position.coords.longitude;
+
+					const initialState = { lng: longitude, lat: latitude, zoom: 14 };
+
+					this.map = new Map({
+						container: this.mapContainer.nativeElement,
+						style: `https://api.maptiler.com/maps/streets-v2/style.json?key=97sou0kVjlk5MxdovBEU `,
+						center: [initialState.lng, initialState.lat],
+						zoom: initialState.zoom
+					});
+
+					this.map?.addControl(new NavigationControl({}), 'top-right');
+				},
+				(error) => {
+					console.log(`Geolocation error: ${error}`);
+					// Handle error case
+				}
+			);
+		} else {
+			console.log('Geolocation is not supported by this browser.');
+			// Handle unsupported case
+		}
 	}
 
 	ngOnDestroy() {
