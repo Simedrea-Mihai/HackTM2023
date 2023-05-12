201b6a8cd1a214e8d7e3c0e430334a474a44477e
Aleksandru <mischie.andrey@gmail.com>
2023-05-13 00:15:03 +0300

    legend search and filter created

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapComponent } from './shared/components/map/map.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FilterButtonComponent } from './filter-button/filter-button.component';
+import { LegendComponent } from './legend/legend.component';
 
 @NgModule({
-	declarations: [AppComponent, MapComponent],
+	declarations: [AppComponent, MapComponent, SearchBarComponent, FilterButtonComponent, LegendComponent],
 	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent]
