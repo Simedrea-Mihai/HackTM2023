1dd598418c7c6a16b9b7a90ba4823582e2786354
adrian-dragos <adrian123795@gmail.com>
2023-05-13 01:21:50 +0300

    add nothing

--- FE/src/app/shared/components/map/map.component.ts
+++ FE/src/app/shared/components/map/map.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ElementRef,
+	AfterViewInit,
+	OnDestroy,
+	ComponentFactoryResolver,
+	ViewContainerRef
+} from '@angular/core';
 import { Map, NavigationControl, Marker, Popup } from 'maplibre-gl';
 import { MapDetailsComponent } from '../map-details/map-details.component';
 
@@ -13,19 +22,15 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 	@ViewChild('map')
 	private mapContainer!: ElementRef<HTMLElement>;
 
-  constructor(
-    private cfr: ComponentFactoryResolver,
-    private vcr: ViewContainerRef
-  ) {}
-  
-  // ...
-  
+	constructor(private cfr: ComponentFactoryResolver, private vcr: ViewContainerRef) {}
+
+	// ...
 
 	ngOnInit(): void {}
 
 	ngAfterViewInit() {
-    const componentFactory = this.cfr.resolveComponentFactory(MapDetailsComponent);
-    const componentRef = componentFactory.create(this.vcr.injector);
+		const componentFactory = this.cfr.resolveComponentFactory(MapDetailsComponent);
+		const componentRef = componentFactory.create(this.vcr.injector);
 
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
@@ -42,16 +47,62 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 						zoom: initialState.zoom
 					});
 
-          var popup = new Popup({ offset: 25 }).setDOMContent(componentRef.location.nativeElement);
-          
-          componentRef.changeDetectorRef.detectChanges();
+					this.map.on('load', () => {
+						this.map?.addSource('maine', {
+							type: 'geojson',
+							data: {
+								type: 'Feature',
+								geometry: {
+									type: 'Polygon',
+									coordinates: [
+										[
+											[-67.13734351262877, 45.137451890638886],
+											[-66.96466, 44.8097],
+											[-68.03252, 44.3252],
+											[-69.06, 43.98],
+											[-70.11617, 43.68405],
+											[-70.64573401557249, 43.090083319667144],
+											[-70.75102474636725, 43.08003225358635],
+											[-70.79761105007827, 43.21973948828747],
+											[-70.98176001655037, 43.36789581966826],
+											[-70.94416541205806, 43.46633942318431],
+											[-71.08482, 45.3052400000002],
+											[-70.6600225491012, 45.46022288673396],
+											[-70.30495378282376, 45.914794623389355],
+											[-70.00014034695016, 46.69317088478567],
+											[-69.23708614772835, 47.44777598732787],
+											[-68.90478084987546, 47.184794623394396],
+											[-68.23430497910454, 47.35462921812177],
+											[-67.79035274928509, 47.066248887716995],
+											[-67.79141211614706, 45.702585354182816],
+											[-67.13734351262877, 45.137451890638886]
+										]
+									]
+								}
+							}
+						});
 
+						this.map?.addLayer({
+							id: 'maine',
+							type: 'fill',
+							source: 'maine',
+							layout: {},
+							paint: {
+								'fill-outline-color': '#088',
+								'fill-opacity': 0.8
+							}
+						});
+					});
+
+					var popup = new Popup({ offset: 25 }).setDOMContent(componentRef.location.nativeElement);
+
+					componentRef.changeDetectorRef.detectChanges();
 
 					this.map?.addControl(new NavigationControl({}), 'top-right');
-          new Marker({color: "#FF0000"})
-            .setLngLat([longitude, latitude])
-            .setPopup(popup) // add popups
-            .addTo(this.map);
+					new Marker({ color: '#FF0000' })
+						.setLngLat([longitude, latitude])
+						.setPopup(popup) // add popups
+						.addTo(this.map);
 				},
 				(error) => {
 					console.log(`Geolocation error: ${error}`);
