28a371e89aa22f2df34a9d217f9ba1ab3703adc1
StefanRudareanu <stefanrudareanu24@gmail.com>
2023-05-13 15:34:36 +0300

    api integration

---
+++ hacktm-backend/controller/eventscontroller.js
@@ -0,0 +1,42 @@
+const evrouter = require("express").Router();
+const EventsModel = require("../models/events");
+const EvModel = new EventsModel();
+evrouter.post("/addevent", async (req, res) => {
+  try {
+    const data = {
+      name: req.body.name,
+      description: req.body.description,
+      link: req.body.link,
+      type: req.body.type,
+      longitude: req.body.longitude,
+      latitude: req.body.latitude,
+      startdate: req.body.startdate,
+      endate: req.body.endate,
+      startime: req.body.startime,
+    };
+    await EvModel.CreateEvent(data);
+    res.status(200).send({ msg: "created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ err: "error" });
+  }
+});
+evrouter.get("/eventype", async (req, res) => {
+  try {
+    const data = await EvModel.GetEventByType(req.body.type, req.body.date);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ msg: "error" });
+  }
+});
+evrouter.get("/eventbydate", async (req, res) => {
+  try {
+    const data = await EvModel.GetEventByDate(req.body.date);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ err: "err" });
+  }
+});
+module.exports = { evrouter, EvModel };
---
+++ hacktm-backend/populatedb.js
@@ -0,0 +1,58 @@
+const options = {
+  provider: "opencage",
+  apiKey: "2250d94d0bef4ea2acc8c4cf2ff38ace",
+  formatter: null,
+};
+
+const NodeGeoCoder = require("node-geocoder");
+const evdata = require("./hacktm-backendev.json");
+const { EvModel } = require("./controller/eventscontroller");
+const { format, parse } = require("date-fns");
+function reverseString(str) {
+  const arrayStrings = str.split("");
+  const reverseArray = arrayStrings.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray;
+}
+const poppulatedb = () => {
+  evdata.data.forEach(async (e) => {
+    const unformatedate = parse(
+      `${e.date.start_date} 2023`,
+      "MMMM dd yyyy",
+      new Date()
+    );
+    const date = format(unformatedate, "yyyy-MM-dd");
+    let hour = "";
+    const strhour = reverseString(e.date.when);
+    let index_space = 0;
+    let index = 0;
+    while (index_space < 2) {
+      if (strhour[index] != " ") {
+        hour = hour + `${strhour[index]}`;
+      } else {
+        index_space++;
+      }
+      index++;
+    }
+    hour = reverseString(hour);
+    const geolocation = await NodeGeoCoder(options).geocode({
+      address: `${e.address[0] + " Timisoara"}`,
+    });
+    try {
+      await EvModel.CreateEvent({
+        name: e.title,
+        description: e.description,
+        link: e.link,
+        type: "1",
+        startdate: date,
+        endate: date,
+        startime: hour,
+        longitude: geolocation[0].longitude,
+        latitude: geolocation[0].latitude,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+module.exports = poppulatedb;
