e12264568cb63679b06abfd97f87c27b87eabe88
adrian-dragos <adrian123795@gmail.com>
2023-05-14 00:24:01 +0300

    code with bugs that I don't see but they exist

--- FE/src/app/bottom-nav/bottom-nav.component.ts
+++ FE/src/app/bottom-nav/bottom-nav.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FilterService } from '../services/filter.service';
+import { Filter } from '../Models/filter.model';
+import { DatePipe } from '@angular/common';
+import { ShowLegendService } from '../services/legen.service';
 
 @Component({
-  selector: 'app-bottom-nav',
-  templateUrl: './bottom-nav.component.html',
-  styleUrls: ['./bottom-nav.component.scss']
+	selector: 'app-bottom-nav',
+	templateUrl: './bottom-nav.component.html',
+	styleUrls: ['./bottom-nav.component.scss']
 })
 export class BottomNavComponent {
+	datePipe: DatePipe = new DatePipe('en-US');
+	currentDate = new Date();
 
+	filterService = inject(FilterService);
+	showLegendService = inject(ShowLegendService);
+
+	resetFilter(): void {
+		const filter: Filter = { official: false, unofficial: false, startDate: null, endDate: null };
+		this.filterService.emitEvent(filter);
+		this.showLegendService.emitEvent(false);
+	}
+
+	initFilter(): void {
+		console.log('initFilter');
+		const filter: Filter = {
+			official: true,
+			unofficial: true,
+			startDate: this.datePipe.transform(this.currentDate, 'yyyy-MM-dd'),
+			endDate: this.datePipe.transform(this.currentDate, 'yyyy-MM-dd')
+		};
+		console.log(filter);
+		this.filterService.emitEvent(filter);
+
+		this.showLegendService.emitEvent(true);
+	}
 }
